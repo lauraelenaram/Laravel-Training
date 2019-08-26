5b94414cf20373a2ffed5dc658a25859f4129e08
Laura <laurajamr@hotmail.com>
2019-08-26 14:51:22 -0500

    Laravel text was changed to Sistema Laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema Laravel
                 </div>
 
                 <div class="links">
