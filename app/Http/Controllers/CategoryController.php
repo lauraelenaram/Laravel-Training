2f75f922bb28c4745cfeb860e61ee22351a1520b
Laura <laurajamr@hotmail.com>
2019-08-28 16:13:34 -0500

    index function

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories= Category::all();
+        return $categories;
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        //
+    }
+
+    public function activate(Request $request)
+    {
+        
+    }
+
+    public function desactivate(Request $request)
+    {
+        
+    }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('content/content');
 });
+
+Route::get('/categoria','CategoryController@index');
