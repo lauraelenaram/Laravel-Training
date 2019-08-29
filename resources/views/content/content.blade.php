1114339ec122ecd3426ce81babe60e9b59f93a78
lauraelenaram <33786737+lauraelenaram@users.noreply.github.com>
2019-08-29 16:12:20 -0500

    Feature/view (#6)
    * categories table created
    
    * index function
    
    * controller created
    
    * errors fixed
    
    * errors fixed
    
    * creation of category.vue
    
    * array in the component
    
    * dynamic category table

--- resources/views/content/content.blade.php
+++ resources/views/content/content.blade.php
@@ -1,11 +1,11 @@
     @extends('principal')
     @section('content')  
         <template v-if="menu==0">
-            <example-component></example-component>
+            <h1>Contenido del menu 0</h1>
         </template>  
 
         <template v-if="menu==1">
-            <h1>Contenido del menu 1</h1>
+            <category-component></category-component>
         </template>
 
         <template v-if="menu==2">
